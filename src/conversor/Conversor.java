87dc94e1820c811e2b334dd2921c5cdbe1db9bc3
Alex Alejandro <t.f.g.12@hotmail.com>
2023-07-09 17:52:05 -0600

    Tester 1

---
+++ src/conversor/Conversor.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package conversor;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Alex
+ */
+public class Conversor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Bievenido al conversor de monedas", "Conversor de Monedas", JOptionPane.WARNING_MESSAGE);
+    }
+
+
+    
+}

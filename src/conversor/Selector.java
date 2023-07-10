176fdb8e30701f99f5b17ed75c505252995d0126
Alex Alejandro <t.f.g.12@hotmail.com>
2023-07-09 19:07:07 -0600

    Menue doble options

--- src/conversor/Selector.java
+++ src/conversor/Selector.java
@@ -5,31 +5,44 @@
 package conversor;
 import javax.swing.JOptionPane;
 import javax.swing.JComboBox;
+import javax.swing.*;
 
 /**
  *
  * @author Alex
  */
 public class Selector {
- public void mostrarMenu() {
- String[] opciones1 = {"Opción 1", "Opción 2", "Opción 3"};
+    public void mostrarMenu() {
+        String[] opciones1 = {"Opción 1", "Opción 2", "Opción 3"};
         String[] opciones2 = {"Opción 4", "Opción 5", "Opción 6"};
 
-        JComboBox<String> comboBox = new JComboBox<>(opciones1);
-        comboBox.addItem(opciones2[0]);
-        comboBox.addItem(opciones2[1]);
-        comboBox.addItem(opciones2[2]);
+        JComboBox<String> comboBox1 = new JComboBox<>(opciones1);
+        JComboBox<String> comboBox2 = new JComboBox<>(opciones2);
 
-        JOptionPane.showMessageDialog(
-            null,
-            comboBox,
-            "Menú",
-            JOptionPane.PLAIN_MESSAGE
+        JPanel panel = new JPanel();
+        panel.add(new JLabel("Opción 1:"));
+        panel.add(comboBox1);
+        panel.add(Box.createHorizontalStrut(15)); // Separación horizontal entre los JComboBox
+        panel.add(new JLabel("Opción 2:"));
+        panel.add(comboBox2);
+
+        int seleccion = JOptionPane.showOptionDialog(
+                null,
+                panel,
+                "Menú",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                new Object[]{},
+                null
         );
 
-        String opcionSeleccionada = (String) comboBox.getSelectedItem();
-        System.out.println("Opción seleccionada: " + opcionSeleccionada);
+        if (seleccion != JOptionPane.CLOSED_OPTION) {
+            String opcionSeleccionada1 = (String) comboBox1.getSelectedItem();
+            String opcionSeleccionada2 = (String) comboBox2.getSelectedItem();
+
+            System.out.println("Opción 1 seleccionada: " + opcionSeleccionada1);
+            System.out.println("Opción 2 seleccionada: " + opcionSeleccionada2);
+        }
     }
 }
-
-
